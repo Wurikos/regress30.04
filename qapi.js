360afb88cf665b147dcfc83210e2fca34a5e69fe
Sulin Alexander <50768680+Wurikos@users.noreply.github.com>
2021-04-30 11:43:20 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"608bc22d2061a7001e175cef",
+				"608bc22d2061a7001e175cf1"
+			]
+		},
+		"608bc22d2061a7001e175cef": {
+			"id": "608bc22d2061a7001e175cef",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"608bc22d2061a7001e175cf1": {
+			"id": "608bc22d2061a7001e175cf1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as QuarklycommunityKitTrack } from "./QuarklycommunityKitTrack"
+export { default as QuarklycommunityKitYouTube } from "./QuarklycommunityKitYouTube"
